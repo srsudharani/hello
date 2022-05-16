0907ed0fc74d20cdd605b6245cf75a73205b2ab4
srsudharani <104753979+srsudharani@users.noreply.github.com>
2022-05-16 14:43:42 +0530

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,10 @@
+package com.sudha.print.package1;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Sudha");
+	}
+
+}
